52465ec01b9e9ee6d7e145e01f0ccedb38170f56
Bryant Finney <bryant.finney@outlook.com>
2024-09-26 10:29:18 -0400

    test(#619): refactor test to strip color codes
    Signed-off-by: Bryant Finney <bryant.finney@outlook.com>

--- tests/acceptance/test_619.py
+++ tests/acceptance/test_619.py
@@ -2,29 +2,45 @@
 
 from __future__ import annotations
 
-import functools
+import re
+from pathlib import Path
 
+import pyspry
 import pytest
+
+import config_ninja
 
 try:
     import sh
 except ImportError:
     pytest.skip('the tests in this module require `sh`', allow_module_level=True)
 
-
-config_ninja = functools.partial(
-    sh.config_ninja,
-    _tty_size=(20, 10000),  # prevent the tty width from influencing the number of lines in the output
-)
+# pylint: disable=redefined-outer-name
 
 
-def test_output_message_per_config() -> None:
+CONFIG_OBJECTS = ['example-local', 'example-local-template']
+
+
+@pytest.fixture
+def settings() -> pyspry.Settings:
+    """Return a dictionary with settings for the test."""
+    return config_ninja.load_settings(Path('config-ninja-settings.yaml'))
+
+
+@pytest.mark.parametrize('key', CONFIG_OBJECTS)
+def test_output_message_per_config(settings: pyspry.Settings, key: str) -> None:
     """Verify that `config-ninja apply` prints a single message for each applied controller."""
     # Arrange
-    config_objects = ['example-local', 'example-local-template']
+    dest = settings.OBJECTS[key]['dest']
+    source = settings.OBJECTS[key]['source']
+    path = source['init' if 'init' in source else 'new']['kwargs']['path']
 
     # Act
-    out = config_ninja('apply', *config_objects)
+    out = sh.config_ninja('apply', *CONFIG_OBJECTS)
+    no_colors = re.sub(r'\x1b\[[01359][35]?m', '', out)
 
     # Assert
-    assert len(config_objects) == len(out.strip().split('\n')), out.strip()
+    assert f'Apply {key}' in no_colors, no_colors
+    assert dest['path'] in no_colors, no_colors
+    assert source['format'] in no_colors, no_colors
+    assert path in no_colors, no_colors
