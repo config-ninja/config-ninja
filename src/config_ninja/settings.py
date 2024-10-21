60c9dbf121575a2fd3ac9b076c4b7bc792c5a9f3
Bryant Finney <bryant.finney@outlook.com>
2024-10-21 08:28:41 -0400

    refactor(#620): move `TypedDict` from `pyspry.pyi`
    locating it in `config_ninja.settings` is more appropriate, as these
        classes define the structure of the configuration file
    
    Signed-off-by: Bryant Finney <bryant.finney@outlook.com>

--- src/config_ninja/settings.py
+++ src/config_ninja/settings.py
@@ -1,5 +1,35 @@
 """Read and deserialize configuration for the `config-ninja`_ agent.
 
+The `typing.TypedDict` classes in this module describe the structure of the `config-ninja` settings file:
+
+```yaml
+CONFIG_NINJA_OBJECTS:
+```
+```yaml
+    example-1:
+      dest:
+        format: templates/settings-subset.toml.j2
+        path: /tmp/config-ninja/local/subset.toml
+
+      source:
+        backend: local
+        format: yaml
+
+        new:
+          kwargs:
+            path: config-ninja-settings.yaml
+```
+
+This YAML file would be parsed into a dictionary of the following structure:
+
+- `CONFIG_NINJA_OBJECTS:`
+  - `example-1:`  (`ConfigNinjaObject`)
+    - `dest:`  (`Dest`)
+      - `...`
+    - `source:`  (`Source`)
+      - `...`
+
+
 .. _config-ninja: https://config-ninja.readthedocs.io/home.html
 """
 
@@ -8,14 +38,34 @@ from __future__ import annotations
 import dataclasses
 import logging
 from pathlib import Path
+from typing import Literal, TypedDict
 
 import jinja2
 import pyspry
-from pyspry import Settings
 
-from config_ninja.backend import DUMPERS, FormatT
+from config_ninja.backend import DUMPERS, Backend, FormatT
+from config_ninja.contrib import get_backend
 
-__all__ = ['DEFAULT_PATHS', 'DestSpec', 'PREFIX', 'load', 'resolve_path']
+try:
+    from typing import TypeAlias
+except ImportError:
+    from typing_extensions import TypeAlias
+
+__all__ = [
+    'ConfigNinjaObject',
+    'DEFAULT_PATHS',
+    'DestSpec',
+    'Dest',
+    'Init',
+    'New',
+    'ObjectSpec',
+    'PREFIX',
+    'PathStr',
+    'SourceSpec',
+    'Source',
+    'load',
+    'resolve_path',
+]
 
 logger = logging.getLogger(__name__)
 
@@ -42,9 +92,9 @@ PREFIX = 'CONFIG_NINJA'
 """
 
 
-def load(path: Path) -> Settings:
+def load(path: Path) -> pyspry.Settings:
     """Load the settings from the given path."""
-    return Settings.load(path, PREFIX)
+    return pyspry.Settings.load(path, PREFIX)
 
 
 def resolve_path() -> Path:
@@ -56,6 +106,146 @@ def resolve_path() -> Path:
     raise FileNotFoundError('Could not find config-ninja settings', DEFAULT_PATHS)
 
 
+class Init(TypedDict):
+    """Initialization parameters for the backend class.
+
+    ```yaml
+    CONFIG_NINJA_OBJECTS:
+      example-1:
+        source:
+    ```
+    ```yaml
+          init:
+            kwargs:
+              path: config-ninja-settings.yaml
+    ```
+    """
+
+    kwargs: dict[str, str]
+    """Pass these values as arguments to the `config_ninja.backend.Backend`'s `__init__()` method."""
+
+
+class New(TypedDict):
+    """Initialization parameters for the backend class.
+
+    ```yaml
+    CONFIG_NINJA_OBJECTS:
+      example-1:
+        source:
+    ```
+    ```yaml
+          new:
+            kwargs:
+              path: config-ninja-settings.yaml
+    ```
+    """
+
+    kwargs: dict[str, str]
+    """Pass these values as arguments to the `config_ninja.backend.Backend.new()` on creation."""
+
+
+class Source(TypedDict):
+    """Describe the source of configuration data written to a `Dest`.
+
+    The parameters `Source.init` and `Source.new` are mutually exclusive.
+
+    ```yaml
+    CONFIG_NINJA_OBJECTS:
+      example-1:
+    ```
+    ```yaml
+        source:
+          backend: local
+          format: yaml
+
+          new:
+            kwargs:
+              path: config-ninja-settings.yaml
+    ```
+    """
+
+    backend: Literal['local', 'appconfig']
+    """The module in `config_ninja.contrib` implementing the `config_ninja.backend.Backend` class."""
+
+    format: FormatT
+    """Deserialize the source data from this format.
+
+    Defaults to `'raw'`.
+    """
+
+    init: Init
+    """Pass these parameters to the `config_ninja.backend.Backend`'s `__init__()` method.
+
+    These are typically unique identifiers; friendly names can be passed to the
+    `config_ninja.backend.Backend.new()` method (via `Source.new`) instead.
+    """
+
+    new: New
+    """Pass these parameters to the backend class's `config_ninja.backend.Backend.new()` method.
+
+    If this property is defined, the `Source.init` property is ignored.
+    """
+
+
+PathStr: TypeAlias = str
+"""A string representing a file path."""
+
+
+class Dest(TypedDict):
+    """Destination metadata for the object's output file.
+
+    ```yaml
+    CONFIG_NINJA_OBJECTS:
+      example-1:
+    ```
+    ```yaml
+        dest:
+          # you can specify the path to a Jinja2 template:
+          format: templates/settings-subset.toml.j2
+          path: /tmp/config-ninja/local/subset.toml
+    ```
+    """
+
+    path: str
+    """Write the configuration file to this path"""
+
+    format: FormatT | PathStr
+    """Set the output serialization format of the destination file.
+
+    If given the path to a file, interpret the file as a Jinja2 template and render it with the
+    source data.
+    """
+
+
+class ConfigNinjaObject(TypedDict):
+    """Specify metadata to manage a system configuration file.
+
+    ```yaml
+    CONFIG_NINJA_OBJECTS:
+    ```
+    ```yaml
+        example-1:
+          dest:
+            format: templates/settings-subset.toml.j2
+            path: /tmp/config-ninja/local/subset.toml
+
+          source:
+            backend: local
+            format: yaml
+
+            new:
+              kwargs:
+                path: config-ninja-settings.yaml
+    ```
+    """
+
+    dest: Dest
+    """Metadata for the object's output file."""
+
+    source: Source
+    """Configuration data for the object's `config_ninja.backend.Backend` data source."""
+
+
 @dataclasses.dataclass
 class DestSpec:
     """Container for the destination spec parsed from `config-ninja`_'s own configuration file.
@@ -63,17 +253,11 @@ class DestSpec:
     .. _config-ninja: https://config-ninja.readthedocs.io/home.html
     """
 
+    format: FormatT | jinja2.Template
+    """Specify the format of the configuration file to write."""
+
     path: Path
     """Write the configuration file to this path."""
-
-    format: FormatT | jinja2.Template
-    """Specify the format of the configuration file to write.
-
-    This property is either a `config_ninja.backend.FormatT` or a `jinja2.environment.Template`:
-    - if `config_ninja.backend.FormatT`, the identified `config_ninja.backend.DUMPERS` will be used
-        to serialize the configuration object
-    - if `jinja2.environment.Template`, this template will be used to render the configuration file
-    """
 
     def __str__(self) -> str:
         """Represent the destination spec as a string."""
@@ -86,7 +270,7 @@ class DestSpec:
         return f'{fmt} -> {self.path}'
 
     @classmethod
-    def from_primitives(cls, data: pyspry.DestType) -> DestSpec:
+    def from_primitives(cls, data: Dest) -> DestSpec:
         """Create a `DestSpec` instance from a dictionary of primitive types."""
         path = Path(data['path'])
         if data['format'] in DUMPERS:
@@ -104,6 +288,58 @@ class DestSpec:
     def is_template(self) -> bool:
         """Whether the destination uses a Jinja2 template."""
         return isinstance(self.format, jinja2.Template)
+
+
+@dataclasses.dataclass
+class SourceSpec:
+    """The data source of a `config-ninja`_ object.
+
+    .. _config-ninja: https://config-ninja.readthedocs.io/home.html
+    """
+
+    backend: Backend
+    """Read configuration data from this backend (see `config_ninja.contrib` for supported backends)."""
+
+    format: FormatT = 'raw'
+    """Decode the source data from this format."""
+
+    @classmethod
+    def from_primitives(cls, data: Source) -> SourceSpec:
+        """Create a `SourceSpec` instance from a dictionary of primitive types.
+
+        If the given `Source` has a `Source.new` key, the appropriate `config_ninja.backend.Backend.new()` method is
+        invoked to create `SourceSpec.backend`. Otherwise, the `Source` must have a `Source.init` key for passing
+        arguments to the `config_ninja.backend.Backend`'s `__init__()` method.
+        """
+        backend_class = get_backend(data['backend'])
+        fmt = data.get('format', 'raw')
+        if new := data.get('new'):
+            backend = backend_class.new(**new['kwargs'])
+        else:
+            backend = backend_class(**data['init']['kwargs'])
+
+        return SourceSpec(backend=backend, format=fmt)
+
+
+@dataclasses.dataclass
+class ObjectSpec:
+    """Container for each object parsed from `config-ninja`_'s own configuration file.
+
+    .. _config-ninja: https://config-ninja.readthedocs.io/home.html
+    """
+
+    dest: DestSpec
+    """Destination metadata for the object's output file."""
+
+    source: SourceSpec
+    """Configuration for the object's `config_ninja.backend.Backend` data source."""
+
+    @classmethod
+    def from_primitives(cls, data: ConfigNinjaObject) -> ObjectSpec:
+        """Create an `ObjectSpec` instance from a dictionary of primitive types."""
+        return ObjectSpec(
+            dest=DestSpec.from_primitives(data['dest']), source=SourceSpec.from_primitives(data['source'])
+        )
 
 
 logger.debug('successfully imported %s', __name__)
