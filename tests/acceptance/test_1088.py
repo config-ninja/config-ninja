e9923b756d138856709b50ae84d10f476596b109
Bryant Finney <bryant.finney@outlook.com>
2025-11-05 14:56:22 -0500

    perf(#1088): implement failing test (TDD)
    Closes config-ninja/config-ninja#1088
    
    Signed-off-by: Bryant Finney <bryant.finney@outlook.com>

---
+++ tests/acceptance/test_1088.py
@@ -0,0 +1,61 @@
+"""Define acceptance tests for caching AppConfig requests."""
+
+from __future__ import annotations
+
+from unittest import mock
+
+import boto3
+import botocore
+import botocore.paginate
+import pytest
+from mypy_boto3_appconfig import AppConfigClient
+
+from config_ninja.contrib.appconfig import AppConfigBackend
+
+
+@pytest.fixture
+def mock_session() -> boto3.Session:
+    """Mock the `AppConfig` session used by the `config_ninja.contrib.appconfig.AppConfigBackend` class."""
+    return mock.MagicMock(spec_set=boto3.Session)
+
+
+@pytest.fixture
+def mock_client(mock_session: mock.MagicMock) -> AppConfigClient:
+    """Create a mocked `AppConfig` client and patch the session to return it."""
+    client = mock.MagicMock(spec_set=AppConfigClient)
+    mock_session.client.return_value = client
+    return client
+
+
+@pytest.fixture
+def mock_paginator(mock_client: mock.MagicMock) -> botocore.paginate.Paginator[str]:
+    """Create a mocked paginator and patch the client to return it."""
+    paginator = mock.MagicMock(spec_set=botocore.paginate.Paginator)
+    mock_client.get_paginator.return_value = paginator
+    return paginator
+
+
+@pytest.fixture
+def mock_page_iterator(mock_paginator: mock.MagicMock) -> botocore.paginate.PageIterator[str]:
+    """Create a mocked page iterator and patch the paginator to return it."""
+    paginated_results = mock.MagicMock(spec_set=botocore.paginate.PageIterator)
+    paginated_results.search.return_value = [f'mock-id-{__name__}']
+    mock_paginator.paginate.return_value = paginated_results
+    return paginated_results
+
+
+def test_cached_appconfig_requests(
+    mock_session: boto3.Session,
+    mock_page_iterator: botocore.paginate.PageIterator[str],
+) -> None:
+    """Verify requests are cached when instantiating the `config_ninja.contrib.appconfig.AppConfigBackend` class."""
+    # Arrange
+    num_instances = 10
+    mock_search: mock.MagicMock = mock_page_iterator.search  # type: ignore[assignment]
+
+    # Act
+    for _ in range(num_instances):
+        AppConfigBackend.new(f'{__name__}_app', f'{__name__}_config', f'{__name__}_env', session=mock_session)
+
+    # Assert
+    assert 1 == mock_search.call_count
